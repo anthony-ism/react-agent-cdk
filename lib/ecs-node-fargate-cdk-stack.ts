2c5aae2b6f3366046617aa516c75c5efca75f9db
Anthony Rizzo <anthony@moven.com>
2025-06-11 16:40:57 -0400

    Adjust changes for deployment

--- lib/ecs-node-fargate-cdk-stack.ts
+++ lib/ecs-node-fargate-cdk-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 
 export class EcsNodeFargateCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsNodeFargateCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+      cluster: cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset('../python-agent'),
+      },
+      publicLoadBalancer: true,
+    });
   }
 }
