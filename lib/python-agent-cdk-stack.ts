0034d1bea7b9306c334746e0821f7e65328e4b3a
Anthony Rizzo <anthony@moven.com>
2025-06-12 05:13:50 -0400

    Initial commit

--- lib/python-agent-cdk-stack.ts
+++ lib/python-agent-cdk-stack.ts
@@ -4,19 +4,19 @@ import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 
-export class EcsNodeFargateCdkStack extends cdk.Stack {
+export class PythonAgentCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'MyVpc', {
+    const vpc = new ec2.Vpc(this, 'PythonAgentVpc', {
       maxAzs: 2
     });
 
-    const cluster = new ecs.Cluster(this, 'MyCluster', {
+    const cluster = new ecs.Cluster(this, 'PythonAgentCluster', {
       vpc: vpc
     });
 
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'MyFargateService', {
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'PythonAgentService', {
       cluster: cluster,
       taskImageOptions: {
         image: ecs.ContainerImage.fromAsset('../python-agent'),
